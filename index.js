e40417df5736485d82015a8b12bfede139bf6576
LeeviKu <leevi.kukkula@gmail.com>
2020-10-07 23:56:21 +0300

    Add new method multiply() to index.js.
    Multiplies input with id "number" with input with id "number2"
    and puts the result to element with id "result".

--- index.js
+++ index.js
@@ -9,3 +9,9 @@ function minus () {
   var number2 = document.getElementById("number2").value
   document.getElementById("result").innerHTML = Number(number) - Number(number2)
 }
+
+function multiply () {
+  var number = document.getElementById("number").value
+  var number2 = document.getElementById("number2").value
+  document.getElementById("result").innerHTML = Number(number) - Number(number2)
+}
