ef11101041b143107a0d4df3dd800826db86a5c7
LeeviKu <leevi.kukkula@gmail.com>
2020-10-07 23:49:43 +0300

    Add new method minus() to index.js.
    Substracts input with id "number2" from input with
    id "number" and puts the result to p element with id
    "result".

--- index.js
+++ index.js
@@ -3,3 +3,9 @@ function sum () {
   var number2 = document.getElementById("number2").value
   document.getElementById("result").innerHTML = Number(number) + Number(number2)
 }
+
+function minus () {
+  var number = document.getElementById("number").value
+  var number2 = document.getElementById("number2").value
+  document.getElementById("result").innerHTML = Number(number) - Number(number2)
+}
