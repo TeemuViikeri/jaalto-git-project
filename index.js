afcfe2f58ffe8eca20a5bf05e4ee753accb67b54
LeeviKu <leevi.kukkula@gmail.com>
2020-10-07 03:31:54 +0300

    Add new method sum() to index.js
    Sum() takes two values from inputs in index.html. Sums them
    and puts the result to p element with id "result"

---
+++ index.js
@@ -0,0 +1,5 @@
+function sum () {
+  var number = document.getElementById("number").value
+  var number2 = document.getElementById("number2").value
+  document.getElementById("result").innerHTML = number + number2
+}
