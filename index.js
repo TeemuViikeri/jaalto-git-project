0e021a322f03759e91ca2085a940da75c5272a54
LeeviKu <leevi.kukkula@gmail.com>
2020-10-08 00:03:09 +0300

    Add new method divide() to index.js
    Divides input with id "number" with input with id "number2"
    and puts the result to element with id "result".

--- index.js
+++ index.js
@@ -15,3 +15,9 @@ function multiply () {
   var number2 = document.getElementById("number2").value
   document.getElementById("result").innerHTML = Number(number) - Number(number2)
 }
+
+function divide () {
+  var number = document.getElementById("number").value
+  var number2 = document.getElementById("number2").value
+  document.getElementById("result").innerHTML = Number(number) / Number(number2)
+}
