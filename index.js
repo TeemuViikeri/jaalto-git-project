4174031d0d06dbdbac606b8d95f4be7b16a51dbc
LeeviKu <leevi.kukkula@gmail.com>
2020-10-08 22:45:04 +0300

    Add string "result: " before the result.

--- index.js
+++ index.js
@@ -1,23 +1,23 @@
 function sum () {
   var number = document.getElementById("number").value
   var number2 = document.getElementById("number2").value
-  document.getElementById("result").innerHTML = Number(number) + Number(number2)
+  document.getElementById("result").innerHTML = "result: " + (Number(number) + Number(number2))
 }
 
 function minus () {
   var number = document.getElementById("number").value
   var number2 = document.getElementById("number2").value
-  document.getElementById("result").innerHTML = Number(number) - Number(number2)
+  document.getElementById("result").innerHTML = "result: " + (Number(number) - Number(number2))
 }
 
 function multiply () {
   var number = document.getElementById("number").value
   var number2 = document.getElementById("number2").value
-  document.getElementById("result").innerHTML = Number(number) * Number(number2)
+  document.getElementById("result").innerHTML = "result: " + (Number(number) * Number(number2))
 }
 
 function divide () {
   var number = document.getElementById("number").value
   var number2 = document.getElementById("number2").value
-  document.getElementById("result").innerHTML = Number(number) / Number(number2)
+  document.getElementById("result").innerHTML = "result: " + (Number(number) / Number(number2))
 }
