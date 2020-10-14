243d83b2d9d4180d4dbf3e41f3dd77567e6ad66b
LeeviKu <leevi.kukkula@gmail.com>
2020-10-14 03:24:12 +0300

    Add matrix for tetris t shaped piece.
    Array of arrays where 1 array represent one line. 0 is for empty spot
    and 1 is for spot that is going to be filled with color.

--- tetris.js
+++ tetris.js
@@ -3,3 +3,9 @@ const context = canvas.getContext('2d')
 
 context.fillStyle = '#000'
 context.fillRect(0, 0, canvas.width, canvas.height)
+
+const matrix = [
+  [0, 0, 0],
+  [1, 1, 1],
+  [0, 1, 0],
+]
