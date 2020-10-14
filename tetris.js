12830addf32cca6d6bc424d2335027c829e29f48
LeeviKu <leevi.kukkula@gmail.com>
2020-10-15 02:51:02 +0300

    Move fillstyle and fillrect to draw().
    Fillstyle and fillrect are needed in start of draw() because the canvas
    needs to be emptied before moving matrix.

--- tetris.js
+++ tetris.js
@@ -3,8 +3,6 @@ const context = canvas.getContext('2d')
 
 context.scale(20, 20)
 
-context.fillStyle = '#000'
-context.fillRect(0, 0, canvas.width, canvas.height)
 
 const matrix = [
   [0, 0, 0],
@@ -13,6 +11,8 @@ const matrix = [
 ]
 
 function draw() {
+  context.fillStyle = '#000'
+  context.fillRect(0, 0, canvas.width, canvas.height)
   drawMatrix(player.matrix, player.pos)
 }
 
