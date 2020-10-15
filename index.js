b23c36033abe6b039dd420e868fc62e03a2d4576
TeemuViikeri <teemu.viikeri@tuni.fi>
2020-10-15 11:01:26 +0300

    Add text formatting to hours and minutes (main.css).
    Previous commit included text formatting (add 0 in front of the seconds
    if it is less than 10) to seconds but not hours and minutes. This commit
    fixes that.

--- index.js
+++ index.js
@@ -42,6 +42,14 @@ function showTime() {
   var minutes = currentTime.getMinutes();
   var seconds = currentTime.getSeconds();
 
+  if (hours <= 10) {
+    hours = '0' + hours;
+  }
+
+  if (minutes <= 10) {
+    minutes = '0' + minutes;
+  }
+
   if (seconds <= 10) {
     seconds = '0' + seconds;
   }
