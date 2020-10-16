619b00c2009a6edbd903e0b9d5a924586b696882
LeeviKu <leevi.kukkula@gmail.com>
2020-10-17 02:54:01 +0300

    Add new function playerDrop().
    Function increments playr.pos.y and resets dropCounter.
    PlayerDrop() is called when player presses down arrow key.

--- tetris.js
+++ tetris.js
@@ -26,6 +26,12 @@ function drawMatrix(matrix, offset) {
     })
   })
 }
+
+function playerDrop() {
+    player.pos.y++;
+    dropCounter = 0 ;
+}
+
 let dropCounter = 0;
 let dropInterval = 1000;
 
@@ -55,7 +61,7 @@ document.addEventListener('keydown', event => {
   } else if (event.keyCode === 39) {
     player.pos.x++;
   } else if (event.keyCode === 40) {
-    
+    playerDrop();
   }
 }) 
 
