d403262746385cfc2c19f19ddd896f4b9b0f829e
TeemuViikeri <teemu.viikeri@tuni.fi>
2020-10-17 10:06:32 +0300

    Fix clock time bug (index.js).

--- index.js
+++ index.js
@@ -42,15 +42,15 @@ function showTime() {
   var minutes = currentTime.getMinutes();
   var seconds = currentTime.getSeconds();
 
-  if (hours <= 10) {
+  if (hours < 10) {
     hours = '0' + hours;
   }
 
-  if (minutes <= 10) {
+  if (minutes < 10) {
     minutes = '0' + minutes;
   }
 
-  if (seconds <= 10) {
+  if (seconds < 10) {
     seconds = '0' + seconds;
   }
 
