f2d662fa1287b58f4ec06154a22cc90f768e2bef
LeeviKu <leevi.kukkula@gmail.com>
2020-10-20 18:39:37 +0300

    Add new function playerMove(dir) to tetris.js.
    PlayerMove() takes a number and increments or decrements
    player.pos.x accordingly.

--- tetris.js
+++ tetris.js
@@ -38,6 +38,13 @@ function playerDrop() {
     dropCounter = 0 ;
 }
 
+function playerMove(dir) {
+  player.pos.x += dir
+  if (collide(arena, player)) {
+    player.pox.x -= dir
+  } 
+}
+
 let dropCounter = 0;
 let dropInterval = 1000;
 
@@ -94,9 +101,9 @@ const player = {
 
 document.addEventListener('keydown', event => {
   if (event.keyCode === 37) {
-    player.pos.x--;
+    playerMove(-1)
   } else if (event.keyCode === 39) {
-    player.pos.x++;
+    playerMove(1)
   } else if (event.keyCode === 40) {
     playerDrop();
   }
