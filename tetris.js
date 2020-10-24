330090fa25ecf35454ad9dd809eada3235b4fc35
LeeviKu <leevi.kukkula@gmail.com>
2020-10-24 07:33:43 +0300

    Modify updateScore() in tetris.js.
    updateScore() now puts String "Score: " to div with id "score" before
    the score amount.

--- tetris.js
+++ tetris.js
@@ -199,7 +199,7 @@ function update(time = 0) {
 }
 
 function updateScore() {
-  document.getElementById("score").innerText = player.score
+  document.getElementById("score").innerText = "Score: " + player.score
 }
 
 const colors = [
