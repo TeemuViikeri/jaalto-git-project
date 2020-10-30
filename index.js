4044ed4d2e66afe0e452fd02f95f9f8e9185a7fc
TeemuViikeri <teemu.viikeri@tuni.fi>
2020-10-30 12:03:38 +0200

    Add new func: createListItem (index.js).
    Function content was taken from additem() func. createListItem() will be
    used in different places, therefore removing duplication via making a
    function was needed.

--- index.js
+++ index.js
@@ -128,22 +128,7 @@ function addItem(event) {
   // This id value will be later used as an attribute id value
   const id = new Date().getTime().toString();
   if (value && !editFlag) {
-    const element = document.createElement("article");
-    element.classList.add("list-item");
-    // data-* attributes are used to store extra information via attributes which scripts can target
-    const attr = document.createAttribute("data-id");
-    attr.value = id;
-    element.setAttributeNode(attr);
-    element.innerHTML = `<p class="title">${value}</p>
-     <div class="btn-container">
-        <button type="button" class="edit-btn"><i class="fas fa-edit"></i></button>
-        <button type="button" class="delete-btn"><i class="fas fa-trash"></i></button>
-     </div>`;
-    const deleteBtn = element.querySelector(".delete-btn");
-    const editBtn = element.querySelector(".edit-btn");
-    deleteBtn.addEventListener("click", deleteItem);
-    editBtn.addEventListener("click", editItem);
-    list.appendChild(element);
+    
     displayAlert("item added to the list", "success");
     container.classList.add("show-container");
 
@@ -258,6 +243,25 @@ function setupItems() {
 
   if (localStorageItems.length > 0) {
   }
+}
+
+function createListItem(id, value) {
+  const element = document.createElement("article");
+    element.classList.add("list-item");
+    // data-* attributes are used to store extra information via attributes which scripts can target
+    const attr = document.createAttribute("data-id");
+    attr.value = id;
+    element.setAttributeNode(attr);
+    element.innerHTML = `<p class="title">${value}</p>
+     <div class="btn-container">
+        <button type="button" class="edit-btn"><i class="fas fa-edit"></i></button>
+        <button type="button" class="delete-btn"><i class="fas fa-trash"></i></button>
+     </div>`;
+    const deleteBtn = element.querySelector(".delete-btn");
+    const editBtn = element.querySelector(".edit-btn");
+    deleteBtn.addEventListener("click", deleteItem);
+    editBtn.addEventListener("click", editItem);
+    list.appendChild(element);
 }
 
 // TIMELINE
